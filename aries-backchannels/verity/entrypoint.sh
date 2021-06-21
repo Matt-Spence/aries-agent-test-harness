dc275cd001e594785d6ec393beaa7287741dddc8
Matthew Spence <Matthew.Spence@evernym.com>
2021-06-21 14:02:19 -0500

    Further work on verity backchannel

--- aries-backchannels/verity/entrypoint.sh
+++ aries-backchannels/verity/entrypoint.sh
@@ -18,18 +18,55 @@ provision() {
   echo
   echo "***************************   PROVISIONING    ************************"
 
-  # If seed was not defined generate a random one
-  if [ -z "$VERITY_SEED" ]; then
-    VERITY_SEED=$(date +%s | md5sum | base64 | head -c 32)
-  fi
-
-  # If network was not defined default to "demo"
+  # AATH requires to be run agains a VON Network
   NETWORK="von"
   TXN_FILE="von.txn"
 
-  # Prepare genesis file and TAA data for the selected network
+  echo
+  echo "*********Env Variables**********"
+  printenv
+  echo
 
-  export TXN_FILE="von.txn"
+  # Handle run mode environment variables
+  run_mode="$RUN_MODE"
+  docker_host="host.docker.internal"
+  if [ ! -z "$DOCKERHOST" ] ; then 
+    docker_host="$DOCKERHOST"
+  fi
+  external_host="localhost"
+  if [ "$run_mode" == "docker" ] ; then
+    external_host=docker_host
+  fi
+
+  # Get the genesis file for the ledger
+  echo
+  echo "Getting Genesis Transactions"
+  echo
+
+  echo "Ledger URL: $LEDGER_URL"
+  ledger_url="$LEDGER_URL"
+  if [ ! -z "$GENESIS_FILE" ] ; then
+    echo "Received Genesis file"
+    echo $GENESIS_FILE > /etc/verity/verity-application/von.txn
+  elif [ ! -z "$GENESIS_URL" ] ; then
+    echo "Retrieving genesis txns from genesis url: ${GENESIS_URL}"
+    curl $GENESIS_URL > /etc/verity/verity-application/von.txn
+  elif [ ! -z "$LEDGER_URL" ] ; then 
+    echo "Retrieving genesis txns from ${ledger_url}/genesis"
+    curl ${ledger_url}/genesis > /etc/verity/verity-application/von.txn
+  else
+    TXN_FILE="local-genesis.txn"
+  fi
+  
+  echo
+  echo "*******GENESIS TRANSACTIONS********"
+  cat /etc/verity/verity-application/von.txn
+  echo
+
+  # Generate a random seed
+  VERITY_SEED=$(date +%s | md5sum | base64 | head -c 32)
+
+  export TXN_FILE
   DATE=$(date +%F)
   export TAA_ACCEPTANCE=$DATE
   export TAA_HASH="N/A"
@@ -37,24 +74,20 @@ provision() {
 
   # Create DID/Verkey based on the provided VERITY_SEED 
   printf "wallet create test key=test\nwallet open test key=test\ndid new seed=%s" "${VERITY_SEED}" | indy-cli | tee /tmp/indy_cli_output.txt
-  echo 
+  echo
 
-  run_mode="$RUN_MODE"
-  docker_host="host.docker.internal"
-  if [ ! -z "$DOCKER_HOST"] ; then 
-    docker_host="$DOCKER_HOST"
-  fi
-  external_host="localhost"
-  if [ run_mode == "docker"] ; then
-    external_host=docker_host
-  fi
   # Register Verity DID/Verkey on the ledger via VON-Network Registration endpoint
-  ledger_url="http://$external_host:9000"
+  ledger_url="http://${external_host}:9000"
   if [ ! -z "$LEDGER_URL" ] ; then 
     ledger_url="$LEDGER_URL"
   fi
-  curl -sd "{\"alias\":\"verity\",\"seed\":$DID,\"verkey\":$VERKEY\"}" ${ledger_url}/register
 
+  DID=`grep verkey /tmp/indy_cli_output.txt |awk '{print $2}'`
+  VERKEY=`grep verkey /tmp/indy_cli_output.txt |awk '{print $7}'`
+
+  echo "Writing did and verkey to ${ledger_url}/register"
+  echo "Posting {\"alias\":\"verity\",\"did\":$DID,\"verkey\":$VERKEY, \"role\":\"ENDORSER\"} to ${ledger_url}/register"
+  curl -d "{\"alias\":\"verity\",\"did\":$DID,\"verkey\":$VERKEY, \"role\":\"ENDORSER\"}" -X POST ${ledger_url}/register
 
   # Write out TAA configruation to the file
   echo "verity.lib-indy.ledger.transaction_author_agreement.agreements = {\"${TAA_VERSION}\" = { digest = \${?TAA_HASH}, mechanism = on_file, time-of-acceptance = \${?TAA_ACCEPTANCE}}}" > /etc/verity/verity-application/taa.conf
@@ -62,13 +95,10 @@ provision() {
   # Generate random logo
   ROBO_HASH=$(date +%s | md5sum | base64 | head -c 8)
   export LOGO_URL="http://robohash.org/${ROBO_HASH}"
-  echo "**********************************************************************"
-  echo
-
 }
 
 start_ngrok() {
-  ngrok http 9000 >> /dev/null &
+  ngrok http ${AGENT_PORT} >> /dev/null &
   NGROK_PID=$!
   until curl -m 1 -q http://127.0.0.1:4040/api/tunnels 2> /dev/null | jq -M -r -e '.tunnels[0].public_url' > /dev/null 2>&1
   do
@@ -77,27 +107,11 @@ start_ngrok() {
   done
 }
 
-save_env() {
-  echo "export VERITY_SEED=$VERITY_SEED" >> provisioned.conf
-  echo "export NETWORK=$NETWORK" >> provisioned.conf
-  echo "export TXN_FILE=${TXN_FILE}" >> provisioned.conf
-  echo "export TAA_VERSION=${TAA_VERSION}" >> provisioned.conf
-  echo "export TAA_HASH=${TAA_HASH}" >> provisioned.conf
-  echo "export TAA_ACCEPTANCE=${TAA_ACCEPTANCE}" >> provisioned.conf
-  echo "export LOGO_URL=${LOGO_URL}" >> provisioned.conf
-  echo "export HOST_ADDRESS=${HOST_ADDRESS}" >> provisioned.conf
-}
-
 print_config() {
   echo "******************        VERITY PARAMETERS         ******************"
   echo "VERITY_SEED=$VERITY_SEED"
   echo "NETWORK=$NETWORK"
   echo "TXN_FILE=${TXN_FILE}"
-  echo "TAA_VERSION=${TAA_VERSION}"
-  echo "TAA_HASH=${TAA_HASH}"
-  echo "TAA_ACCEPTANCE=${TAA_ACCEPTANCE}"
-  echo "LOGO_URL=${LOGO_URL}"
-  echo "HOST_ADDRESS=${HOST_ADDRESS}"
   echo "**********************************************************************"
   echo
 }
@@ -116,42 +130,29 @@ start_verity() {
 
   print_license
 
-  if [ -f provisioned.conf ]; then
-    # Provisioning file exists. This is start of the stopped Verity container
-    # Source environment variables from the previous run
-    . provisioned.conf
-    export BOOTSTRAP="done"
-  else
-    # First time strart. Do the provisioning
-    provision
-    save_env
-  fi
+  provision
 
   print_config
 
   echo "**********************       VERITY STARTUP         ******************"
   # If public URL for docker Host is not specified start Ngrok tunnel to obtain public Verity Application endpoint
-  if [ -z "$HOST_ADDRESS" ]; then
-    echo "No HOST_ADDRESS specified"
-    echo -n Starting ngrok..
-    start_ngrok
-    export HOST_ADDRESS=$(curl -m 1 -s http://127.0.0.1:4040/api/tunnels 2> /dev/null | jq -M -r '.tunnels[0].public_url')
-  fi
+  echo -n Starting ngrok..
+  start_ngrok
+  export HOST_ADDRESS=$(curl -m 1 -s http://127.0.0.1:4040/api/tunnels 2> /dev/null | jq -M -r '.tunnels[0].public_url')
 
   export HOST_DOMAIN=`echo $HOST_ADDRESS |  cut -d'/' -f3`
 
   echo
   printf "Verity Endpoint is: ${ANSII_GREEN}${HOST_ADDRESS}${ANSII_RESET}"
   echo
-  echo
 
   # Start Verity Application
   /usr/bin/java -cp /etc/verity/verity-application:.m2/repository/org/fusesource/leveldbjni/leveldbjni-all/1.8/leveldbjni-all-1.8.jar:/usr/lib/verity-application/verity-application-assembly.jar \
-  com.evernym.verity.Main &> log.txt &
+  com.evernym.verity.Main &
 
   echo
-  echo -n "Waiting for Verity to start listening."
-  until curl -q 127.0.0.1:9000/agency > /dev/null 2>&1
+  echo -n "Waiting for Verity to start listening"
+  until curl 127.0.0.1:${AGENT_PORT}/agency > /dev/null 2>&1
   do
       echo -n "."
       sleep 1
@@ -163,11 +164,11 @@ start_verity() {
     echo "Bootstrapping Verity"
     echo
     echo "Verity Setup"
-    curl -f -H "Content-Type: application/json" -X POST http://127.0.0.1:9000/agency/internal/setup/key \
+    curl -f -H "Content-Type: application/json" -X POST http://127.0.0.1:${AGENT_PORT}/agency/internal/setup/key \
     -d "{\"seed\":\"$VERITY_SEED\"}" || exit 1
     echo; echo
     echo "Verity Endpoint Setup"
-    curl -f -X POST http://127.0.0.1:9000/agency/internal/setup/endpoint || exit 1
+    curl -f -X POST http://127.0.0.1:${AGENT_PORT}/agency/internal/setup/endpoint || exit 1
     echo; echo
     echo "Verity Bootstrapping complete."
   fi
@@ -175,10 +176,7 @@ start_verity() {
   echo "Verity application started."
   echo "**********************************************************************"
   echo
-
-  tail -f -n +1 log.txt
 }
 
 start_verity
 
-node verity_backchannel.js
--- aries-backchannels/verity/verity_backchannel.js
+++ aries-backchannels/verity/verity_backchannel.js
@@ -1,24 +1,18 @@
 "use strict";
-import minimist from "minimist";
+const minimist = require("minimist");
 //Express
 const axios = require('axios')
 const bodyParser = require('body-parser')
 const express = require("express");
-const session = require("express-session");
-const fs = require('fs');
-const sdk = require('verity-sdk')
-const request = require('request-promise-native');
+const sdk = require('verity-sdk');
+const shell = require('shelljs')
+const ngrok = require('ngrok');
+const base64url = require('base64url');
 
-const CONTEXT_PATH = './verity_config/verity-context.json'
-const CONFIG_PATH = './verity_config/config.json'
+const WALLET_NAME = `Wallet-${Math.floor(Math.random() * (999999 - 100000) + 100000)}`;
+const WALLET_KEY = `Key-${Math.floor(Math.random() * (999999 - 100000) + 100000)}`;
 
-const config = JSON.parse(fs.readFileSync(CONFIG_PATH))
-
-const WALLET_NAME = config.WALLET.NAME
-const WALLET_KEY = config.WALLET.KEY
-const TOKEN = ""    // No provision token necessary on local verity instance
-const VERITY_URL = ""
-const LISTENING_PORT = config.APP.LISTENING_PORT
+var context = null
 
 async function main() {
     const cliArguments = minimist(process.argv.slice(2), {
@@ -30,48 +24,16 @@ async function main() {
         },
     });
 
-    const VERITY_URL = process.env.HOST_ADDRESS
+    console.log('Environment Variables:')
+    console.log(process.env)
 
-    const backchannelPort = Number(cliArguments.port);
-    const agentPort = backchannelPort + 1;
-    const dockerHost = process.env.DOCKERHOST ?? "host.docker.internal";
-    const runMode = process.env.RUN_MODE;
-    const externalHost = runMode === "docker" ? dockerHost: "localhost";
+    const BACKCHANNEL_PORT = Number(cliArguments.port)
+    const AGENT_PORT = BACKCHANNEL_PORT+1
+    process.env.AGENT_PORT = AGENT_PORT
 
-    const endpointUrl = `http://${externalHost}`
+    var VERITY_URL = null
 
-    const genesisFile = process.env.GENESIS_FILE;
-    const genesisUrl = process.env.GENESIS_URL;
-    const ledgerUrl = process.env.LEDGER_URL ?? `http://${externalHost}:9000`;
-    // won't be necessary until more functionality is implemented
-    // const genesisPath = await getGenesisPath(
-    //     genesisFile,
-    //     genesisUrl,
-    //     ledgerUrl,
-    //     dockerHost
-    // );
-
-    async function get_genesis_txns(){
-        var genesis
-        if (genesisUrl){
-            axios.get(`http://dev.bcovrin.vonx.io/genesis`)
-            .then(function (response) {
-                console.log(response)
-                genesis = response.body
-            })
-        }
-        else if (runMode == "docker"){
-            axios.get(`${ledgerUrl}/genesis`)
-            .then(function (response) {
-                console.log(response)
-                genesis = response.body
-            })
-        } else {
-            genesis = fs.readFileSync("../local-genesis.txt")
-        }
-    }
-
-    genesis = await get_genesis_txns()
+    var backchannel_url = `127.0.0.1:${BACKCHANNEL_PORT}`
 
     const app = express()
     app.use(express.text({
@@ -80,102 +42,153 @@ async function main() {
         }
     }))
 
-    app.use(session({secret:'SHHHHHHHHH'}))
-
     function defaultHandler (message) {
         console.log(`Unhandled message:${message}`)
     }
 
     const handlers = new sdk.Handlers()
     handlers.setDefaultHandler(defaultHandler)
+    console.log('set default handler')
 
     const connectionsMap = {}
 
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function attempt(func, attempts, params = []) {
+        var i
+        for(i = 0; i < attempts; i=i+1) {
+            var result = await func(params)
+            if(result != null) {
+                return result
+            }
+            await sleep(1000)
+        }
+        return null
+    }
+    async function getVerityUrl() {
+        let result
+        console.log('Retrieving Verity ngrok url')
+        await axios.get('http://127.0.0.1:4040/api/tunnels', {timeout: 2})
+            .then((response) => {
+                result = response.data.tunnels[0].public_url
+            }).catch((error) => {
+                console.log('Could not get verity url')
+                result = null
+            })
+        return result
+    }
+
     async function provisionAgent () {
         // create initial Context
-        var ctx = await sdk.Context.create(WALLET_NAME, WALLET_KEY, VERITY_URL, '')
-        console.log('wallet created')
-        const provision = new sdk.protocols.v0_7.Provision(null, TOKEN)
-
+        VERITY_URL = await attempt(getVerityUrl, 10)
+        console.log(`Provisioning agent with Verity at ${VERITY_URL}`)
+        console.log('Creating Verity Context')
+        console.log(`Wallet name: ${WALLET_NAME}`)
+        console.log(`Wallet key: ${WALLET_KEY}`)
+        const ctx = await sdk.Context.create(WALLET_NAME, WALLET_KEY, VERITY_URL, '')
+        console.log(`context created:`)
+        console.log(ctx)
+        const provision = new sdk.protocols.v0_7.Provision(null, null)
+        console.log('Provisioning new agent')
         try {
             const context = await provision.provision(ctx)
-            fs.writeFileSync(CONTEXT_PATH, JSON.stringify(context.getConfig()))  
-            console.log(`Context written to file: ${JSON.stringify(context.getConfig())}`)
+            console.log('Agent Provisioned')
             return context
         } catch (e) {
             console.log(e)
         }
     }
 
-    async function updateConfigs() {
-        context.endpointUrl = `http://${externalHost}:${agentPort}/webhook`
+    async function updateEndpoint () {
+        console.log(`Updating webhook endppoint to ${backchannel_url}/webhook`)
+        context.endpointUrl = `${backchannel_url}/webhook`
+
+        // request that verity application use specified webhook endpoint
+        await new sdk.protocols.UpdateEndpoint().update(context)
     }
 
-    const context = await provisionAgent()
-    await UpdateConfigs()
-
-    async function handleConnectionPostOperation(operation, data, res, id=None) {
+    async function handleConnectionPostOperation(operation, data, res, id=null) {
+        var result
         switch (operation) {
             case 'create-invitation': 
                 result = await createInvitation()
-                res.setStatus(200).send(`{"connection_id": "${result[0]}", "state": "N/A", "invitation": "${JSON.stringify(result[1])}"`)
+                console.log(`sending response: \n {"connection_id": \"${result[0]}\", \"state\": \"N/A\", \"invitation\": ${JSON.stringify(result[1])}}`)
+                res.status(200).send(`{"connection_id": \"${result[0]}\", \"state\": \"N/A\", \"invitation\": ${JSON.stringify(result[1])}}`)
                 break;
             case 'receive-invitation': 
                 result = await receiveInvitation(data)
+                console.log(`Mapping ${result[1]} to ${result[0]}`)
                 connectionsMap[result[0]] = result[1]
-                res.setStatus(200).send('{"state":"N/A"')
+                console.log(`sending response: \n {"state":"N/A"}`)
+                res.status(200).send(`{"state":"N/A", "connection_id": \"${result[0]}\"}`)
                 break;
             case 'accept-invitation': 
                 result = await acceptInvitation(data, id)
-                res.setStatus(501).send()
+                res.status(501).send()
                 break;
             case 'send-ping':
-                res.sendStatus(501).send()
+                res.status(501).send()
                 break;
                 // return await sendPing();
             default: 
-                res.sendStatus(501).send()
+                res.status(501).send()
+                break;
         }
     }
 
     app.post('/agent/command/:topic/:operation', async function(req, res) {
         switch(req.params.topic) {
-            case 'Connection':
-                await handleConnectionPostOperation(req.params.operation, req.body.data, res, req.body.id)
+            case 'connection':
+                console.log(`Matched operation: ${req.params.operation}`)
+                console.log(JSON.parse(req.body).data)
+                await handleConnectionPostOperation(req.params.operation, JSON.parse(req.body).data, res, req.body.id)
                 break;
             default:
-                res.sendStatus(501).send()
+                console.log(`Matched operation: ${req.params.operation}`)
+                res.status(501).send()
                 break;
         }
     })
 
     async function createInvitation() {
-        const relationship = new sdk.protocols.v1_0.Relationship(None, None, None, None, None)
-        const relationshipKeys = new Promise((resolve) => {
+        const relationship = new sdk.protocols.v1_0.Relationship(null, null, null, null, null)
+        const relationshipProtocol = new Promise((resolve) => {
             handlers.addHandler(relationship.msgFamily, relationship.msgFamilyVersion, async (msgName, message) => {
                 switch(msgName) {
                     case relationship.msgNames.CREATED:
                         console.log(message)
-                        const threadId = message['~thread'].thid
-                        var relDID = message.did
-                        const verKey = message.verKey
+                        // const threadId = message['~thread'].thid
+                        const relDID = message.did
+                        // const verKey = message.verKey
                         console.log(`relationship DID: ${relDID}`)
-                        console.log(`relationship verKey: ${message.verKey}`)
-                        //TODO: save these values in the database
-                        resolve([relDID, threadId])
+                        // console.log(`relationship verKey: ${message.verKey}`)
+                        resolve(message.did)
                         break
                     case relationship.msgNames.CONNECTION_INVITATION:
                         resolve(message.inviteURL)
+                        break
                 }
             })
         })
+
+        if(!context) {
+            context = await provisionAgent()
+        }
+        await updateEndpoint()
+        console.log("Backchannel webhook endpoint updated")
+
+        console.log("creating relationsip")
+
+        let relDid
         relationship.create(context)
-        await relationshipKeys
+        await relationshipProtocol.then((value) => {
+            relDid = value
+        })
+        console.log(`relationship Did created: ${relDid}`)
 
-        relDid = relationshipKeys[0]
-        threadId = relationshipKeys[1]
-
-        const invitation = new Promise((resolve) => {
+        const createInvitation = new Promise((resolve) => {
             handlers.addHandler(relationship.msgFamily, relationship.msgFamilyVersion, async (msgName, message) => {
                 switch(msgName) {
                     case relationship.msgNames.INVITATION:
@@ -187,41 +200,57 @@ async function main() {
         })
 
         await relationship.connectionInvitation(context)
-        await invitation
+        let invitation
+        await createInvitation.then((value) => {
+            invitation = value
+        })
 
-        return [relDID, invitation]
+        console.log(`Invitation created`)
+        console.log(invitation)
+
+        return [relDid, invitation]
     }
 
     async function receiveInvitation(data) {
-        const relationship = new sdk.protocols.v1_0.Relationship(None, None, None, None, None)
+        console.log(`Recieved invitation: ${JSON.stringify(data)}`)
+        const relationship = new sdk.protocols.v1_0.Relationship()
         const relationshipKeys = new Promise((resolve) => {
             handlers.addHandler(relationship.msgFamily, relationship.msgFamilyVersion, async (msgName, message) => {
                 switch(msgName) {
                     case relationship.msgNames.CREATED:
                         console.log(message)
-                        const threadId = message['~thread'].thid
                         var relDID = message.did
-                        const verKey = message.verKey
-                        resolve([relDID, threadId])
+                        resolve(relDID)
                         break
                     case relationship.msgNames.CONNECTION_INVITATION:
                         resolve(message.inviteURL)
                 }
             })
         })
-        relationship.create(context)
-        await relationshipKeys
+        await relationship.create(context)
+        console.log('relationship created')
+        let relDid
+        await relationshipKeys.then((result) => {
+            relDid = result
+        })
 
-        relDid = relationshipKeys[0]
-        threadId = relationshipKeys[1]
-        connection = sdk.protocols.v1_0.Connecting(relDid, "label", `https://vas.pps.evernym.com:443/agency/msg?c_i=${base64url.encode(JSON.stringify(data))}`)
-        return [relDid, connection]
+        const inviteUrl = `${VERITY_URL}:${AGENT_PORT}/agency/msg?c_i=${base64url.encode(JSON.stringify(data))}`
+        console.log(`Generated Invite Url: ${inviteUrl}`)
+        await axios.get(inviteUrl, {timeout: 2}).then((response) => {
+            console.log("Retrieved data from invite url:")
+            console.log(response.data)
+        }).catch((error) => {
+            console.log(`Error retrieving invite from inviteUrl`)
+        })
+        return [relDid, inviteUrl]
     }
 
-    async function acceptInvitation(data, connection_id=None) {
-        connecting = connectionsMap[connection_id]
+    async function acceptInvitation(data, connection_id=null) {
+        console.log(`Accepting invitation request, data parameter: ${data}`)
+        const invitation = connectionsMap[connection_id]
+        const connectionProtocol = new sdk.protocols.v1_0.Connecting(connection_id, "label", invitation)
         const connection = new Promise((resolve) => {
-            handlers.addHandler(Connecting.msgFamily, Connecting.msgFamilyVersion, async (msgName, message) => {
+            handlers.addHandler(connectionProtocol.msgFamily, connectionProtocol.msgFamilyVersion, async (msgName, message) => {
                 switch(msgName) {
                     case 'accept':
                         console.log(message)
@@ -233,32 +262,75 @@ async function main() {
                 
             })
         })
-        connecting.accept(context)
+        await connectionProtocol.accept(context)
+        await connection
+    }
+
+    async function handleConnectionGetOperation(id=null) {
+        // TODO: See if there's a way to get the status of a specific verity agent. Might not be practical
+
+        if(id != null) {
+            return "N/A"
+        }
+        return "N/A"
     }
 
 
     app.get('/agent/command/:topic', async function(req, res) {
         switch(req.params.topic) {
-            case 'Connection': 
-                handler = new connectionHandler()
-                result = await handler.handleGetOperation(req.params.topic, id)
-                res.setStatus(200).send(`"state":"${result}"}`)
+            case 'connection': 
+                console.log(`Matched operaiton: ${req.params.topic}`)
+                result = await handleConnectionGetOperation()
+                res.status(200).send(`"state":"${result}"}`)
+                break;
+            case 'status':
+                console.log(`Matched operaiton: ${req.params.topic}`)
+                var url = await attempt(getVerityUrl, 10)
+                var status = await attempt(checkVerity, 1, [url])
+                if(status != null) {
+                    res.status(200).send()
+                }
+                res.status(404).send()
+                break;
+            case 'version':
+                console.log(`Matched operaiton: ${req.params.topic}`)
+                res.status(200).send("1.0")
+                break;
+            default:
+                console.log(`Unimplemented operation: ${req.params.topic}`)
+                res.status(501).send()
+
         }
     })
 
     app.get('/agent/command/:topic/:id', async function(req, res) {
-        id = req.params.id
         switch(req.params.topic) {
-            case 'Connection': 
-                handler = new connectionHandler()
-                result = await handler.handleGetOperation(req.params.topic, id)
-                res.setStatus(200).send(`"state":"${result}"}`)
+            case 'connection': 
+                console.log(`Matched operaiton: ${req.params.topic}, ${req.params.id}`)
+                const result = await handleConnectionGetOperation(req.params.id)
+                res.status(200).send(`{"state":"${result}"}`)
         }
     })
 
-    app.listen(LISTENING_PORT, () => {
-        console.log(`Listening on port ${LISTENING_PORT}`)
+    app.post('/webhook', async function(req, res) {
+        console.log(`webhook recieved request:`)
+        console.log(req.body)
+        await handlers.handleMessage(context, Buffer.from(req.body, 'utf8'))
+        res.status(200).send(`success`)
+    });
+
+    app.listen(BACKCHANNEL_PORT, () => {
+        console.log(`Verity Backchannel listening on port ${BACKCHANNEL_PORT}`)
     })
+
+    async function checkVerity(url=`http://localhost:${AGENT_PORT}/agency`) {
+        console.log(`Checking for process listening on port ${AGENT_PORT}`)
+        const result = await shell.exec(`netstat -tulp | grep \'${AGENT_PORT}\'`).stdout
+        console.log(result)
+        return (result != "") ? true : null
+    }
+
+    shell.exec('./entrypoint.sh', {async:true})
 }
 
 main()
